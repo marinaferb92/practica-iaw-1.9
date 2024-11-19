3654f6901ee6790b846f98839c668e8647e050d2
Ubuntu <ubuntu@ip-172-31-17-37.ec2.internal>
2024-11-19 12:14:05 +0000

    actualizado

---
+++ conf/wp-config.php
@@ -0,0 +1,8 @@
+<?php
+// Configuración de base de datos
+define('DB_NAME', 'BDmfb');
+define('DB_USER', 'mfb92');
+define('DB_PASSWORD', '1234');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
---
+++ scripts/setup_letsencrypt_certificate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Importamos el archivo de variables
+
+source .env
+
+# Configuramos para mostrar los comandos 
+set -ex
+
+#Instalamos y acctualizamos snapd
+snap install core
+snap refresh core
+
+#Eliminamos si existiese alguna instalación previa de certbot con apt
+sudo apt remove certbot -y
+
+#Instalamos el cliente de Certbot con snapd
+ snap install --classic certbot
+
+#Obtenemos el certificado y configuramos el servidor web Apache.
+sudo certbot --apache -m $LE_EMAIL --agree-tos --no-eff-email -d $LE_DOMAIN --non-interactive
+
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
