c18181a3ca8a39052f210c1b692b09ece9a0985c
Ubuntu <ubuntu@ip-172-31-18-240.ec2.internal>
2024-12-06 10:08:04 +0000

    actualizando

--- conf/wp-config.php
+++ conf/wp-config.php
@@ -1,8 +1,10 @@
 <?php
-// Configuración de base de datos
-define('DB_NAME', 'BDmfb');
-define('DB_USER', 'mfb92');
-define('DB_PASSWORD', '1234');
-define('DB_HOST', 'localhost');
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
+
+// Datos de conexión a la base de datos
+define('DB_HOST', '172.31.18.110'); // IP del servidor MySQL (BACKEND_PRIVATE_IP)
+define('DB_NAME', 'wordpress_db'); // Nombre de la base de datos (WORDPRESS_DB_NAME)
+define('DB_USER', 'user');         // Usuario de la base de datos (WORDPRESS_DB_USER)
+define('DB_PASSWORD', 'user');     // Contraseña del usuario (WORDPRESS_DB_PASSWORD)
+
+// Conexión a la base de datos
+$mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
